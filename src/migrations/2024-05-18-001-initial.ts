06a9d815eae7f786cea10ccc3a5d8292ab829aa7
Brayden Langley <brayden167@gmail.com>
2024-07-12 11:06:03 -0700

    Test using direct migrations

--- src/migrations/2024-05-18-001-initial.ts
+++ src/migrations/2024-05-18-001-initial.ts
@@ -1,16 +1,30 @@
-import { Knex } from 'knex'
-import { KnexStorageMigrations } from '@bsv/overlay'
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  KnexStorageMigrations.default[0].up(knex)
-  knex.schema.createTable('lookup_data', t => {
-    t.string('txid')
-    t.integer('outputIndex')
-    t.binary('script')
+  await knex.schema.createTable('outputs', table => {
+    table.increments()
+    table.string('txid', 64)
+    table.integer('outputIndex', 10)
+    table.binary('outputScript')
+    table.string('topic')
+    table.integer('satoshis', 15)
+    table.binary('beef')
+    // Represents the outputs that were provided as inputs
+    // to the transaction that created this output.
+    // This indicates the correct history of this output.
+    table.text('outputsConsumed', 'longtext').defaultTo('[]')
+    // Tracks any outputs the current output is used as an input in it's creation
+    table.text('consumedBy', 'longtext').defaultTo('[]')
+    table.boolean('spent').defaultTo(false)
+  })
+  await knex.schema.createTable('applied_transactions', table => {
+    table.increments()
+    table.string('txid', 64)
+    table.string('topic')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.dropTable('lookup_data')
-  KnexStorageMigrations.default[0].down(knex)
+  await knex.schema.dropTable('applied_transactions')
+  await knex.schema.dropTable('outputs')
 }
